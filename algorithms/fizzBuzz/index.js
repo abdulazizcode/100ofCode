09ab5df922bcbfd987ff4dbe8e6e95c7d8538295
abdulazizcode <tajikcoder@gmail.com>
2021-02-23 20:37:52 +0800

    [ADD]- fizbazz algorithm

---
+++ algorithms/fizzBuzz/index.js
@@ -0,0 +1,9 @@
+if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+} else if (i % 3 === 0) {
+    console.log("Fizz");
+} else if (i % 5 === 0) {
+    console.log("Buzz");
+} else {
+    console.log(i);
+}
