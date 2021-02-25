4b964e3321a5200143218423fd1a427dfc7e09b2
abdulazizcode <tajikcoder@gmail.com>
2021-02-26 00:14:46 +0800

    [MOD]- adding steps for range function

--- algorithms/rangeStartEnd/index.js
+++ algorithms/rangeStartEnd/index.js
@@ -1,9 +1,18 @@
-function  range(start,end){
-   let newArr = [];
-   for(let i = start; i<=end; i++){
-        newArr.push(i);
-}
-  return newArr;
+function  range(start,end,step){
+    if(step == null) step = 1;
+    let newArr = [];
+
+      if(step > 0){
+	 for(let i = start; i<=end; i+= step){
+            newArr.push(i);
+       }
+      }else{
+	  for(let i = start; i>=end; i++)
+              newArr.push(i);
+      }
+      return newArr;
 }
 
-console.log(range(1,10));
+console.log(range(1,10,2)); //custom step
+console.log(range(1,10)); //default step
+
