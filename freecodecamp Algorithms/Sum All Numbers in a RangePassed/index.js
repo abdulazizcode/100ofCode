260b7a4564ef0045f2478c3e18eb4a4ea373a9b7
abdulazizcode <tajikcoder@gmail.com>
2021-03-04 02:30:46 +0800

    [INT]- freecodecamp Algorithms

---
+++ freecodecamp Algorithms/Sum All Numbers in a RangePassed/index.js
@@ -0,0 +1,24 @@
+/*
+We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. 
+The lowest number will not always come first.
+For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
+
+*/
+
+
+function sumAll(arr){
+	// Find min and max numbers in array
+	const max = Math.max(...arr);
+	const min = Math.min(...arr);
+
+	// variable to hold our sum
+	let sum = 0;
+
+	// loop and add all numbers from lowest to highest value
+	for(let i = min; i <=max; i++){
+		sum += i;
+	}
+	return sum;
+}
+
+console.log(sumAll([1,4]));
