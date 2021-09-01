e7ef09b5b7aaaa3ad6c8e95adcbf543a676c316a
Abdusamadzoda Abdulaziz <67753244+abdulazizcode@users.noreply.github.com>
2021-09-01 15:35:49 +0800

    o(n)

--- algorithms/array_common_iteam/index.js
+++ algorithms/array_common_iteam/index.js
@@ -19,3 +19,29 @@ const compareFindMath = (arr1,arr2) =>{
 }
 
 console.log(compareFindMath(arr1,arr2));
+
+
+
+const secondAproach = (arr1,arr2) =>{
+  let obj = {};
+  
+  for(let i=0; i<arr1.length; i++){
+    if(!obj[arr1[i]]){
+      const element = arr1[i];
+      obj[element] = true;
+    }
+  }
+  
+  for(let j =0; j<arr2.length; j++){
+    if(obj[arr2[j]]){
+      return true
+    }
+  }
+  return false
+}
+
+
+
+const array1 = ['a', 'd', 'm', 'x'];
+const array2 = ['p', 'y', 'k','a'];
+console.log(secondAproach(array1,array2));
