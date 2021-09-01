852a20656b0b3c1bed762a4c3ebe76a4bb3044eb
Abdusamadzoda Abdulaziz <67753244+abdulazizcode@users.noreply.github.com>
2021-09-01 11:07:59 +0800

    Update index.js

--- algorithms/removeDuplicateFromArr/index.js
+++ algorithms/removeDuplicateFromArr/index.js
@@ -7,3 +7,24 @@ console.log(Array.from(new Set(numbers)));
 //const filtered__array = [...new Set(numbers)];
 //console.log(filtered__array);
 
+
+
+let chars = ['A', 'B', 'A', 'C', 'B'];
+
+const uniqueChars = [...new Set(chars)];
+console.log(uniqueChars);
+
+const unique = chars.filter((el,index) =>{
+  return chars.indexOf(el) === index
+})
+console.log(unique);
+
+let uniqueChar = [];
+chars.forEach((c) => {
+    if (!uniqueChar.includes(c)) {
+        uniqueChar.push(c);
+    }
+});
+
+console.log(uniqueChars);
+
